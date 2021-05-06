83205fe2e1aee3422db0f7adcc24d009035e37fc
mtosatti33 <m_tosatti@yahoo.com.br>
2021-05-06 19:46:56 -0300

    added drafts.ini

--- udm.pas
+++ udm.pas
@@ -5,7 +5,11 @@ unit UDM;
 interface
 
 uses
-  Classes, SysUtils, ZConnection, ZDataset;
+  Classes, SysUtils, ZConnection, ZDataset, IniFiles;
+
+const
+  MAIN = 'main';
+  INI_FILE = 'Drafts.ini';
 
 type
 
@@ -23,18 +27,51 @@ type
     qryTeamName: TZQuery;
     qryTeamCount: TZQuery;
     qryTeamAV: TZQuery;
+    procedure DataModuleCreate(Sender: TObject);
   private
-
+    procedure ReadIniFile;
   public
 
   end;
 
+  TIniStrings = record
+   database: string;
+   libraryLoc: string;
+   protocol: string;
+  end;
+
 var
   dm: Tdm;
-
+  ini : TIniFile;
+  iniStrings: TIniStrings;
 implementation
 
 {$R *.lfm}
+
+
+{ Tdm }
+
+procedure Tdm.DataModuleCreate(Sender: TObject);
+begin
+  ReadIniFile;
+
+  conn.Database:=iniStrings.database;
+  conn.LibraryLocation:=iniStrings.libraryLoc;
+  conn.Protocol:=iniStrings.protocol;
+end;
+
+procedure Tdm.ReadIniFile;
+begin
+   ini := TIniFile.Create(INI_FILE);
+   try
+     iniStrings.database:=ini.ReadString(MAIN,'database','');
+     iniStrings.libraryLoc:=ini.ReadString(MAIN,'library','');
+     iniStrings.protocol:=ini.ReadString(MAIN,'protocol','');
+
+   finally
+     ini.Free;
+   end;
+end;
 
 end.
 
